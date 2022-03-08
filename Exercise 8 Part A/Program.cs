59e5b99ce0502742d10d108cbabd3a515fe5866a
Jason <Jason@DESKTOP-RFS54IF>
2022-03-09 11:24:00 +1300

    Add project files.

---
+++ Exercise 8 Part A/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Exercise 8 Part A */
+
+Console.WriteLine("This Program Will Write Even Numbers From 1-100");
+int counter = 1;
+
+//While Counter Is < 101 It Will Increment By 1, If A Number Divided By 2 == 0 Then It Will Print
+while (counter < 101)
+{
+    if (counter % 2 == 0)
+    {
+        Console.WriteLine(counter);
+    }
+    counter++;
+}
